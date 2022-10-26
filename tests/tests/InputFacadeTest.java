68f6c05c409c5bdccb88458a4452d481ded804f6
mandatory-username <106693160+hannaking@users.noreply.github.com>
2022-10-26 16:20:56 -0400

    class path fix

--- tests/tests/InputFacadeTest.java
+++ tests/tests/InputFacadeTest.java
@@ -1,5 +1,30 @@
 package tests;
 
-public class InputFacadeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+public class InputFacadeTest
+{
+
+	// extractFigure
+	// toGeometryRepresentation
+	
+	/*
+	 * 
+	 */
+	@Test
+	void testExtractFigure()
+	{
+		
+	}
+	
+	/*
+	 * 
+	 */
+	@Test
+	void testToGeometryRepresentation()
+	{
+		
+	}
 }
